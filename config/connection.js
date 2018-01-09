f3e9efda4e8042e8a1aa6c6ce44ec572b9943e2c
realonlytomd <mclaughlintomd@gmail.com>
2018-01-08 18:06:33 -0600

    fixing heroku push

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up connection to the mysql database
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  })}
+
 
 // then make the connection
 connection.connect(function(err) {
